91a1b372c17302de77215a5887117cd33a0d88d6
Gary Moore <madmangaz@gmail.com>
2021-02-19 16:20:56 +0000

    Initial Commit

---
+++ Accelerometry/AccelerometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccelerometryApp.swift
+//  Accelerometry
+//
+//  Created by Gary Moore on 19/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct AccelerometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accelerometry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accelerometry
+//
+//  Created by Gary Moore on 19/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
