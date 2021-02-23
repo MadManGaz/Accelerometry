0d530a1c22b60aa9b71f003a31f9868b14e3f065
Gary Moore <madmangaz@gmail.com>
2021-02-23 20:45:46 +0000

    Fix binding for IP address and add 3D
    To clarify, I've added a model to be used with scenekit. This
    will be used for feedback of device movements when the
    accelerometer is turned on.

--- Accelerometry/Accelerometer/AccelerometerController.swift
+++ Accelerometry/Accelerometer/AccelerometerController.swift
@@ -14,11 +14,12 @@ class AccelerometerController: ObservableObject {
     
     private let motionManager = CMMotionManagerSingleton.motionManager
     
-    private var webSocket = AccelerometerWebSocket(url: URL(string: "ws://192.168.1.119:8765")!)
+    private lazy var webSocket = AccelerometerWebSocket(url: URL(string: "ws://0.0.0.0:8765")!)
     
     @Published private(set) var x = 0.0
     @Published private(set) var y = 0.0
     @Published private(set) var z = 0.0
+    @Published private(set) var rotationMatrix = CMRotationMatrix()
     
     private(set) var coordinates = (x: 0.0, y: 0.0, z: 0.0) {
         didSet {
@@ -29,26 +30,34 @@ class AccelerometerController: ObservableObject {
         }
     }
     
-    func startUpdate() {
+    private func startRotationMatrixUpdates(motionManager: CMMotionManager) {
+        
+    }
+    
+    func startUpdate(url: URL) {
         if motionManager.isAccelerometerAvailable {
             motionManager.accelerometerUpdateInterval = 1.0 / 60.0
             motionManager.startAccelerometerUpdates()
             logger.info("Did start accelerometer updates")
             
-            motionManager.startAccelerometerUpdates(to: .main) { (accelerometerData, error) in
+            let queue = OperationQueue()
+            
+            motionManager.startAccelerometerUpdates(to: queue) { (accelerometerData, error) in
                 guard error == nil else {
                     print(error!)
                     return
                 }
                 
                 if let accelData = accelerometerData {
-                    self.x = accelData.acceleration.x
-                    self.y = accelData.acceleration.y
-                    self.z = accelData.acceleration.z
-                    self.coordinates = (self.x, self.y, self.z)
+                    DispatchQueue.main.async {
+                        self.x = accelData.acceleration.x
+                        self.y = accelData.acceleration.y
+                        self.z = accelData.acceleration.z
+                        self.coordinates = (self.x, self.y, self.z)
+                    }
                 }
             }
-            
+            webSocket = AccelerometerWebSocket(url: url)
             webSocket.connect()
         }
     }
